f1e4ee0e617a109e3e82960e6fe2a6155470f073
Lu1sEnriquez <luis.lavz.2300@gmail.com>
2024-02-13 00:27:50 -0700

    se agrego el modulo item

---
+++ src/item/item.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ItemService } from './item.service';
+import { ItemDTO, ItemUpdateDTO } from './dto/item.dto';
+
+@Controller('items')
+export class ItemController {
+  constructor(private readonly itemService: ItemService) {}
+
+  @Post('create')
+  async createCategory(@Body() unidad: ItemDTO) {
+    return this.itemService.createItem(unidad);
+  }
+
+  @Put('update')
+  async updateCategory(@Body() categoria: ItemUpdateDTO) {
+    return this.itemService.updateItem(categoria);
+  }
+
+  @Get('all')
+  async findCategories() {
+    return this.itemService.findItems();
+  }
+
+  @Get(':id')
+  async findCategoryById(@Param('id') id: number) {
+    return this.itemService.findItemById(id);
+  }
+  @Delete(':id')
+  async deleteItemById(@Param('id') id: number) {
+    return this.itemService.deleteItem(id);
+  }
+}
