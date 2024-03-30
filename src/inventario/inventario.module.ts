6e005ac0cfab22bd5c1cb20caca2813a1b932493
EruvielGS <131491666+EruvielGS@users.noreply.github.com>
2024-03-30 01:06:07 -0600

    se importo el modulo HistorialStockModule

--- src/inventario/inventario.module.ts
+++ src/inventario/inventario.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { InventarioService } from './services/inventario.service';
 import { InventarioController } from './controllers/inventario.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { HistorialStockModule } from 'src/historial-stock/historial-stock.module';
 
 @Module({
   providers: [InventarioService],
   controllers: [InventarioController],
-  imports: [PrismaModule],
+  imports: [PrismaModule, HistorialStockModule],
   exports: [InventarioService],
 })
 export class InventarioModule {}
