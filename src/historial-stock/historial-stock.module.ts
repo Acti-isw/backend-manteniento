046fc2e74a15f4311337b8be7d2857b37ad3fda1
EruvielGS <131491666+EruvielGS@users.noreply.github.com>
2024-03-30 01:02:48 -0600

    se exporto el servicio de inventario

--- src/historial-stock/historial-stock.module.ts
+++ src/historial-stock/historial-stock.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   controllers: [HistorialStockController],
   providers: [HistorialStockService],
   imports: [PrismaModule],
+  exports: [HistorialStockService]
 })
 export class HistorialStockModule {}
