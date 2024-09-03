e7469cd85704f4b440706123939c7c078694557d
Lu1sEnriquez <luis.lavz.2300@gmail.com>
2024-09-03 16:45:08 -0700

    format

--- src/unidades/dto/unidades.dto.ts
+++ src/unidades/dto/unidades.dto.ts
@@ -1,54 +1,61 @@
-import { Unidades } from "@prisma/client";
-import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { Unidades } from '@prisma/client';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
-export class UnidadesDTO implements Unidades{
-    @IsOptional()
-    @IsNumber()
-    idUnidad: number;
+export class UnidadesDTO implements Unidades {
+  @IsOptional()
+  @IsNumber()
+  idUnidad: number;
 
-    @IsNotEmpty()
-    @IsString()
-    nombre: string;
+  @IsNotEmpty()
+  @IsString()
+  nombre: string;
 
-    @IsOptional()
-    @IsDate()
-    createAT: Date;
+  @IsOptional()
+  @IsDate()
+  createAT: Date;
 
-    @IsOptional()
-    @IsDate()
-    updateAT: Date;
+  @IsOptional()
+  @IsDate()
+  updateAT: Date;
 
-    @IsOptional()
-    @IsBoolean()
-    isDelete: boolean;
+  @IsOptional()
+  @IsBoolean()
+  isDelete: boolean;
 
-    @IsOptional()
-    @IsNumber()
-    idUsuario: number;
+  @IsOptional()
+  @IsNumber()
+  idUsuario: number;
 }
 
-export class UnidadesUpdateDTO implements Unidades{
-    @IsNotEmpty()
-    @IsNumber()
-    idUnidad: number;
+export class UnidadesUpdateDTO implements Unidades {
+  @IsNotEmpty()
+  @IsNumber()
+  idUnidad: number;
 
-    @IsOptional()
-    @IsString()
-    nombre: string;
+  @IsOptional()
+  @IsString()
+  nombre: string;
 
-    @IsOptional()
-    @IsDate()
-    createAT: Date;
+  @IsOptional()
+  @IsDate()
+  createAT: Date;
 
-    @IsOptional()
-    @IsDate()
-    updateAT: Date;
+  @IsOptional()
+  @IsDate()
+  updateAT: Date;
 
-    @IsOptional()
-    @IsBoolean()
-    isDelete: boolean;
+  @IsOptional()
+  @IsBoolean()
+  isDelete: boolean;
 
-    @IsOptional()
-    @IsNumber()
-    idUsuario: number;
+  @IsOptional()
+  @IsNumber()
+  idUsuario: number;
 }
