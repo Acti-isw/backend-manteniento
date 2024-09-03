6c1df29d7b20c1f417df28940528d309f110348d
Lu1sEnriquez <luis.lavz.2300@gmail.com>
2024-09-03 16:47:17 -0700

    add colums of Empleado in the view

--- prisma/views/dbo/visSalidaEntradaHerramientas.sql
+++ prisma/views/dbo/visSalidaEntradaHerramientas.sql
@@ -12,6 +12,7 @@ SELECT
   cat.nombre AS nombreCategoria,
   cat.idCategoria,
   seh.idEmpleado,
+  emp.Nombre + ' ' + emp.ApellidoPaterno + ' ' + emp.ApellidoMaterno AS nombreEmpleado,
   seh.isSalida,
   seh.idUsuario,
   usu.usuario
@@ -30,5 +31,6 @@ FROM
   AND seh.createAT = max_seh.max_createAT
   LEFT JOIN [dbo].Categoria AS cat ON cat.idCategoria = her.idCategoria
   LEFT JOIN [dbo].Usuarios AS usu ON usu.idUsuario = seh.idUsuario
+  LEFT JOIN [dbo].Empleados AS emp ON emp.idEmpleado = seh.idEmpleado
 WHERE
   her.isDelete = 0;
