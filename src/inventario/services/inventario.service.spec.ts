59eb03c972e81956095b5797276f013a9b4290d3
p-omartineza <p-omartineza@izzi.mx>
2024-10-27 20:14:15 -0700

    firt testing with  jest

--- src/inventario/services/inventario.service.spec.ts
+++ src/inventario/services/inventario.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InventarioService } from './inventario.service';
+import { PrismaService } from '../../prisma/prisma.service';
+
+import { NotFoundException } from '@nestjs/common';
+import { InventarioDTO } from '../dto/inventario.dto';
 
 describe('InventarioService', () => {
   let service: InventarioService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InventarioService],
+      providers: [
+        InventarioService,
+        {
+          provide: PrismaService,
+          useValue: {
+            inventario: {
+              create: jest.fn(), // Inicializamos como un mock
+              update: jest.fn(),
+            },
+            visInventario: {
+              findMany: jest.fn(),
+              findFirst: jest.fn(),
+              count: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<InventarioService>(InventarioService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createInventory', () => {
+    it('should create an inventory', async () => {
+      const inventarioDTO = {
+        /* datos de ejemplo */
+      };
+      (prisma.inventario.create as jest.Mock).mockResolvedValue(inventarioDTO);
+
+      expect(
+        await service.createInventory(inventarioDTO as InventarioDTO),
+      ).toEqual(inventarioDTO);
+      expect(prisma.inventario.create).toHaveBeenCalledWith({
+        data: inventarioDTO,
+      });
+    });
+  });
+
+  describe('findInventoryById', () => {
+    it('should return an inventory if found', async () => {
+      const mockInventory = { idInventario: 1 };
+      (prisma.visInventario.findFirst as jest.Mock).mockResolvedValue(
+        mockInventory,
+      );
+
+      expect(await service.findInventoryById(1)).toEqual(mockInventory);
+    });
+
+    it('should throw NotFoundException if no inventory is found', async () => {
+      (prisma.visInventario.findFirst as jest.Mock).mockResolvedValue(null);
+
+      await expect(service.findInventoryById(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
--- src/inventario/services/inventario.service.ts
+++ src/inventario/services/inventario.service.ts
@@ -1,5 +1,5 @@
 import { Body, Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { InventarioDTO } from '../dto/inventario.dto';
 import { Inventario, visInventario } from '@prisma/client';
 import { isEmpty, pick } from 'lodash';
